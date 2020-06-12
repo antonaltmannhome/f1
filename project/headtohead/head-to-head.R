0dce85a9524c54dfa2cd4335621bbee5ab5f5eda
antonaltmannhome <58005258+antonaltmannhome@users.noreply.github.com>
2020-06-12 16:26:58 +0100

    Update head-to-head.R

--- project/headtohead/head-to-head.R
+++ project/headtohead/head-to-head.R
@@ -7,80 +7,97 @@ lbl = lazy_left_join(lbl, rddf, c('race', 'driver'), 'team')
 
 HeadToHead = function(yearFilter, myDriv1, myDriv2, myTeam) {
   
-  # same as above but make presentation more attractive
-  myLbl = lbl %>%
-    filter(driver %in% c(myDriv1, myDriv2) & year %in% yearFilter & !isRogue & team == myTeam)
-  
-  colToSpread = c('sec', 'inTraffic', 'isCarProblem', 'fuelTyreEffect', 'modalFinPosProb', 'tyreLap')
-  horizLbl = myLbl %>%
-    select(race, lap, driver, colToSpread) %>%
-    spread_multiple(keyCol = driver, sec, inTraffic, isCarProblem, fuelTyreEffect, modalFinPosProb, tyreLap)
-  
-  ## horrible names of course, so rename
-  badName = paste0('driver', apply(expand.grid(c(myDriv1, myDriv2), colToSpread), 1, paste, collapse = '_'))
-  goodName = apply(expand.grid(c(1, 2), colToSpread), 1, function(x) paste(rev(x), collapse = ''))
-  
-  for (j in 1:length(badName)) {
-    names(horizLbl)[names(horizLbl) == badName[j]] = goodName[j]
-  }
-  
-  # so, comparison 1, compare all laps, raw sec
+  tlabb1 = toupper(substr(with(driverDF, surname[driver == myDriv1]), 1, 3))
+  tlabb2 = toupper(substr(with(driverDF, gsub(' ', '', surname[driver == myDriv2])), 1, 3))
   
   numComparison = 5
   compName = c('all laps', 'exclude car problem', 'exclude traffic', 'exclude dead rubber', 'adjust for model')
   deltaVec = rep(NA, numComparison)
   numObVec = rep(NA, numComparison)
   
-  for (i in 1:numComparison) {
-    if (i == 1) {
-      validHorizLbl = horizLbl %>%
-        filter(!is.na(sec1) & !is.na(sec2))
-    }
-    if (i == 2) {
-      validHorizLbl = horizLbl %>%
-        filter(!is.na(sec1) & !is.na(sec2) &
-                 !isCarProblem1 & !isCarProblem2)
-    }
-    if (i == 3) {
-      validHorizLbl = horizLbl %>%
-        filter(!is.na(sec1) & !is.na(sec2) &
-                 !isCarProblem1 & !isCarProblem2 &
-                 !inTraffic1 & !inTraffic2)
-    }
-    if (i %in% c(4, 5)) {
-      validHorizLbl = horizLbl %>%
-        filter(!is.na(sec1) & !is.na(sec2) &
-                 !isCarProblem1 & !isCarProblem2 &
-                 !inTraffic1 & !inTraffic2 &
-                 modalFinPosProb1 < 0.99 & modalFinPosProb2 < 0.99)
-    }
-    
-    dum = validHorizLbl %>%
-      summarise(delta = mean(sec1 - sec2),
-                numLap = n())
-    deltaVec[i] = dum %>% pull(delta)
-    numObVec[i] = dum %>% pull(numLap)
-    
-    if (i == 5) {
-      validHorizLbl = validHorizLbl %>%
-        mutate(adjSec1 = sec1 - fuelTyreEffect1,
-               adjSec2 = sec2 - fuelTyreEffect2)
-      deltaVec[i] = with(validHorizLbl, mean( (adjSec1 - adjSec2)))
-      meanTyreLap1 = mean(validHorizLbl$tyreLap1)
-      meanTyreLap2 = mean(validHorizLbl$tyreLap2)
-    }
+  # same as above but make presentation more attractive
+  myLbl = lbl %>%
+    filter(driver %in% c(myDriv1, myDriv2) & year %in% yearFilter & !isRogue & team == myTeam)
+  # edge case, maybe only one driver appears in that e.g. lotterer's caterham year
+  
+  haveValidComparisonData = length(unique(myLbl$driver)) == 2
+  
+  if (!haveValidComparisonData) {
+    meanTyreLap1 = NA
+    meanTyreLap2 = NA
+    toReturn = lst(tlabb1, tlabb2, deltaVec, numObVec, meanTyreLap1, meanTyreLap2)
   }
   
-  toReturn = MakeListForFunctionOutput(deltaVec, numObVec, meanTyreLap1, meanTyreLap2)
+  if (haveValidComparisonData) {
+    
+    colToSpread = c('sec', 'inTraffic', 'isCarProblem', 'fuelTyreEffect', 'modalFinPosProb', 'tyreLap')
+    horizLbl = myLbl %>%
+      select(race, lap, driver, colToSpread) %>%
+      spread_multiple(keyCol = driver, sec, inTraffic, isCarProblem, fuelTyreEffect, modalFinPosProb, tyreLap)
+    
+    ## horrible names of course, so rename
+    badName = paste0('driver', apply(expand.grid(c(myDriv1, myDriv2), colToSpread), 1, paste, collapse = '_'))
+    goodName = apply(expand.grid(c(1, 2), colToSpread), 1, function(x) paste(rev(x), collapse = ''))
+    
+    for (j in 1:length(badName)) {
+      names(horizLbl)[names(horizLbl) == badName[j]] = goodName[j]
+    }
+    
+    # so, comparison 1, compare all laps, raw sec
+    
+    
+    for (i in 1:numComparison) {
+      if (i == 1) {
+        validHorizLbl = horizLbl %>%
+          filter(!is.na(sec1) & !is.na(sec2))
+      }
+      if (i == 2) {
+        validHorizLbl = horizLbl %>%
+          filter(!is.na(sec1) & !is.na(sec2) &
+                   !isCarProblem1 & !isCarProblem2)
+      }
+      if (i == 3) {
+        validHorizLbl = horizLbl %>%
+          filter(!is.na(sec1) & !is.na(sec2) &
+                   !isCarProblem1 & !isCarProblem2 &
+                   !inTraffic1 & !inTraffic2)
+      }
+      if (i %in% c(4, 5)) {
+        validHorizLbl = horizLbl %>%
+          filter(!is.na(sec1) & !is.na(sec2) &
+                   !isCarProblem1 & !isCarProblem2 &
+                   !inTraffic1 & !inTraffic2 &
+                   modalFinPosProb1 < 0.99 & modalFinPosProb2 < 0.99)
+      }
+      
+      dum = validHorizLbl %>%
+        summarise(delta = mean(sec1 - sec2),
+                  numLap = n())
+      deltaVec[i] = dum %>% pull(delta)
+      numObVec[i] = dum %>% pull(numLap)
+      
+      if (i == 5) {
+        validHorizLbl = validHorizLbl %>%
+          mutate(adjSec1 = sec1 - fuelTyreEffect1,
+                 adjSec2 = sec2 - fuelTyreEffect2)
+        deltaVec[i] = with(validHorizLbl, mean( (adjSec1 - adjSec2)))
+        meanTyreLap1 = mean(validHorizLbl$tyreLap1)
+        meanTyreLap2 = mean(validHorizLbl$tyreLap2)
+      }
+    }
+    
+    toReturn = lst(tlabb1, tlabb2, deltaVec, numObVec, meanTyreLap1, meanTyreLap2)
+  }
+  
   return(toReturn)
 }
 
 MakeReadableComparison = function(yearFilter, myDriv1, myDriv2, myTeam) {
   myRawOutput = HeadToHead(yearFilter, myDriv1, myDriv2, myTeam)
-  mySurname1 = f1data:::GetOtherDriverName(myDriv1)
-  mySurname2 = f1data:::GetOtherDriverName(myDriv2)
+  myTlabb1 = f1data:::GetOtherDriverName(myDriv1)
+  myTlabb2 = f1data:::GetOtherDriverName(myDriv2)
   myAgeComment = paste0('average age of ',
-                        c(mySurname1, mySurname2), '\'s tyres: ',
+                        c(myTlabb1, myTlabb2), '\'s tyres: ',
                         round(c(myRawOutput$meanTyreLap1, myRawOutput$meanTyreLap2), 1),
                         ' laps (during those ', tail(myRawOutput$numObVec, 1), ' laps)')
   
@@ -93,8 +110,8 @@ MakeReadableComparison = function(yearFilter, myDriv1, myDriv2, myTeam) {
                         numOfLaps = c(paste(myRawOutput$numObVec, 'laps'),
                                       rep('', 2)),
                         result = c(paste(ifelse(myRawOutput$deltaVec > 0,
-                                                mySurname2,
-                                                mySurname1),
+                                                myTlabb2,
+                                                myTlabb1),
                                          round(abs(myRawOutput$deltaVec), 2),
                                          'faster'),
                                    rep('', 2)))
@@ -102,12 +119,31 @@ MakeReadableComparison = function(yearFilter, myDriv1, myDriv2, myTeam) {
   return(myReadableTB)
 }
 
+MakeH2HTibbleOutput = function(myDescription, myList) {
+  h2hOutputToVector = function(x)  {
+    c(
+      paste0(ifelse(x$deltaVec > 0, x$tlabb2, x$tlabb1), ' ',
+           round(ifelse(x$deltaVec > 0, x$deltaVec, -x$deltaVec), 2),
+           ' faster (', x$numObVec, ' laps)'),
+      paste0(x$tlabb1, ': ', round(x$meanTyreLap1, 1), ' laps, ',
+             x$tlabb2, ': ', round(x$meanTyreLap2, 1), ' laps (for the ', tail(x$numObVec, 1), ' laps)')
+    )
+  }
+  myTB = t(sapply(myList, h2hOutputToVector))
+  colnames(myTB) = paste0('tempCol', 1:ncol(myTB))
+  myTB = as_tibble(myTB)
+  
+  myTB = bind_cols(myDescription, myTB)
+  
+  return(myTB)
+}
+
 ViewAllHeadToHeadByDriver = function(myDriv1) {
   # these are all the team pairings involving this driver
   myTeamMate = f1data:::GetAllTeamMateByDriver(myDriv1)
   
   yearByYearList = vector('list', nrow(myTeamMate))
-  for (j in 1:nrow(collapsedTeamMate)) {
+  for (j in 1:nrow(myTeamMate)) {
     yearByYearList[[j]] = with(myTeamMate[j,],
                               HeadToHead(yearFilter = year,
                                          myDriv1 = d1,
@@ -131,6 +167,54 @@ ViewAllHeadToHeadByDriver = function(myDriv1) {
   }
   # now the tricky bit, need to make pretty table output
   # but we want 1 row for every season plus an additional row for combined where necessary
+  # let's make the tibble firstly
+
+  yearByYearTB = MakeH2HTibbleOutput(myTeamMate, yearByYearList)
+  collapsedTB = MakeH2HTibbleOutput(collapsedTeamMate %>%
+                                      select(-yearFilter), collapsedList)
   
-  combinedDF = 
+  # now the really fiddly bit, putting them in order
+  maxYearByPairing = myTeamMate %>%
+    group_by(team, d1, d2) %>%
+    summarise(maxYear = max(year))
+  
+  collapsedTB = left_join(collapsedTB, maxYearByPairing, c('team', 'd1', 'd2'))
+  yearByYearTB$maxYear = yearByYearTB$year
+
+  collapsedTB$isSummary = TRUE
+  yearByYearTB$isSummary = FALSE
+  
+  combinedTB = bind_rows(yearByYearTB,
+                         collapsedTB) %>%
+    arrange(maxYear, isSummary)
+  
+  combinedTB$year2 = with(combinedTB, ifelse(isSummary, 'overall', year))
+  combinedTB$teamMate = with(driverDF, surname[match(combinedTB$d2, driver)])
+  
+  cleanCombinedTB = combinedTB %>%
+    mutate_cond(isSummary,
+                team = '',
+                teamMate = '') %>%
+    select(year2, team, teamMate, starts_with('tempCol')) %>%
+    rename(year = year2)
+  
+  return(cleanCombinedTB)
+}
+
+# now the fun bit, making the gt table
+
+MakeH2HGtTable = function(myDriv1) {
+  h2hTB = ViewAllHeadToHeadByDriver(myDriv1)
+  h2hTB %>%
+    gt %>%
+    tab_style(
+      style = list(
+        cell_fill(color = 'lightcyan')
+      ),
+      locations = cells_body(
+        columns = vars(team),
+        rows = (team =='force india')
+      )
+    )
+  # ok, we're on our way
 }
